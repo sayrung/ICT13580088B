2dc75c11b58bbf512ccfc7de855846bad3f31b87
ploytisapcwt <pcwt.ti@hotmail.com>
2017-12-12 16:10:19 +0700

    Sayrung

---
+++ ICT13580088B/MyPage3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ICT13580088B
+{
+    public partial class MyPage3 : ContentPage
+    {
+		public MyPage3()
+		{
+			InitializeComponent();
+			backButton.Clicked += BackButton_Clicked;
+			backToRootButton.Clicked += BackToRootButton_Clicked;
+		}
+
+		void BackButton_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PopModalAsync();
+		}
+
+		void BackToRootButton_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PopToRootAsync();
+		}
+	}
+}
